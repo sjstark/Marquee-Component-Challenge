e38db57c98295b0f29145df520a377c50d3f4bad
Sam <samstarkscenic@gmail.com>
2021-04-06 14:26:53 -0400

    ran build

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import LayoutUI from '../LayoutUI'
 import PageUI from '../PageUI'
--- src/utils/email.js
+++ src/utils/email.js
@@ -6,6 +6,7 @@ We were looking through your coding challenge and are pretty impressed. Do you h
 const emailHref = `mailto:${emailAddress}?subject=${encodeURI(subject)}&body=${encodeURI(body)}`
 
 const handleContact = () => {
+  // A simple easter egg function
   window.open(emailHref, "_blank")
 }
 
